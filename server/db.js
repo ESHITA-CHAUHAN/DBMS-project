3b7eb529ce729be52ef0c568d50e85a4240e5d65
AymaanDhingra <Aymaandhingra2316@gmail.com>
2026-04-25 20:18:17 +0530

    Make SQLite optional for cloud deploys

--- server/db.js
+++ server/db.js
@@ -1,13 +1,12 @@
-const createPostgresStore = require("./db/postgres-store");
-const createSqliteStore = require("./db/sqlite-store");
-
 function selectStore() {
   if (process.env.DATABASE_URL) {
+    const createPostgresStore = require("./db/postgres-store");
     return createPostgresStore({
       connectionString: process.env.DATABASE_URL,
       sslMode: process.env.DATABASE_SSL || ""
     });
   }
+  const createSqliteStore = require("./db/sqlite-store");
   return createSqliteStore();
 }
 
